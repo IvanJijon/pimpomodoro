ae1d63f2cd6c3eed70c39b26c21822602a6e9856
Ivan Jijon <ivanjijon@hotmail.com>
2026-04-04 01:21:36 -0500

    feat: Enable alternate screen support for the timer UI.
    WithAltScreen starts the program with the alternate screen buffer enabled (i.e. the program starts in full window mode). Note that the altscreen will be automatically exited when the program quits.

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	p := tea.NewProgram(tui.NewModel())
+	p := tea.NewProgram(tui.NewModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Oops! There's been an error: %v", err)
 		os.Exit(1)
